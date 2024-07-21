3fec74be38af9d64c354264a4337d431965e4f62
masrayfa <maselon2030@gmail.com>
2024-07-21 12:00:21 +0700

    add db init

--- init.sql
+++ init.sql
@@ -1,1 +1,39 @@
 CREATE DATABASE delos;
+
+-- Create Users table
+CREATE TABLE Users (
+    userId SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Create Farms table
+CREATE TABLE Farms (
+    farmId SERIAL PRIMARY KEY,
+    userId INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT fk_user
+      FOREIGN KEY(userId) 
+	  REFERENCES Users(userId)
+);
+
+-- Create Ponds table
+CREATE TABLE Ponds (
+    pondId SERIAL PRIMARY KEY,
+    farmId INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT fk_farm
+      FOREIGN KEY(farmId) 
+	  REFERENCES Farms(farmId)
+);
